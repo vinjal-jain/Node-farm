467b2da8432095687c44d26c1d0ac57869c5be1b
vinjal-jain <vinjaljain14it@gmail.com>
2023-10-12 14:15:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const textIn = fs.readSync('./txt/input.txt','utf-8');
+console.log(textIn);
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('File written!');
