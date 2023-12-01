69b8728d915ea28d7f5deaba118699eae601f2a2
vinjal-jain <vinjaljain14it@gmail.com>
2023-12-01 14:41:00 +0530

    Prasing Variables from urls

--- index.js
+++ index.js
@@ -57,10 +57,11 @@ const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+ const {query,pathname} = url.parse(req.url, true);
+  
 
   //Overview page 
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
      res.writeHead(200,{'Content-type' : 'text/html'});
        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard,el)).join('');  
       const output = tempOverview.replace('{%PRODUCTS_CARDS%}',cardsHtml);
@@ -68,11 +69,14 @@ const server = http.createServer((req, res) => {
       res.end(output);
 
     //Product page 
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT');
+  } else if (pathname === '/product') {
+    res.writeHead(200,{'Content-type' : 'text/html'}); 
+    const product = dataObj[query.id];
+    const output = replaceTemplate (tempProduct,product); 
+    res.end(output);
 
     //API
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200,{'Content-type' : 'application/json'});
     res.end(data);
 
